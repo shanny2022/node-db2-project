1a0ac02f885a09eafbe5d2fb2d064377c03113a6
shanny2022 <115324786+shanny2022@users.noreply.github.com>
2023-12-26 00:57:18 -0500

    Update cars-router.js

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,45 +1,34 @@
-const express = require('express');
-const Cars = require('./cars-model'); // Assuming you have a cars-model.js file that exports the model functions
-const router = express.Router();
+const Cars = require("./cars-model");
+const router = require("express").Router();
+const {
+    checkCarId,
+    checkCarPayload,
+    checkVinNumberValid,
+    checkVinNumberUnique
+} = require("./cars-middleware");
 
-
-// [POST] /api/cars
-router.post('/', async (req, res) => {
-    const { vin, make, model, mileage } = req.body;
-    if (!vin || !make || !model || !mileage) {
-      return res.status(400).json({ message: 'vin, make, model and mileage are required' });
-    }
-    try {
-      const newCar = await Cars.create(req.body);
-      res.status(201).json(newCar);
-    } catch (err) {
-      res.status(500).json({ message: 'Failed to create new car' });
-    }
-  });
-// [GET] /api/cars
-router.get('/', async (req, res) => {
-  try {
-    const cars = await Cars.getAll(); // Assuming you have a getAll function in your model
-    res.status(200).json(cars);
-  } catch (err) {
-    res.status(500).json({ message: 'Failed to get cars' });
-  }
+router.get("/", (req, res, next) => {
+    Cars.getAll()
+        .then( cars => res.json(cars))
+        .catch(next);
 });
 
-// [GET] /api/cars/:id
-router.get('/:id', async (req, res) => {
-  const { id } = req.params;
-  try {
-    const car = await Cars.getById(id); // Assuming you have a getById function in your model
-    if (car) {
-      res.status(200).json(car);
-    } else {
-      res.status(404).json({ message: 'Could not find car with given id.' })
-    }
-  } catch (err) {
-    res.status(500).json({ message: 'Failed to get car' });
-  }
+router.get("/:id", checkCarId, (req, res, next) => {
+    Cars.getById(req.params.id)
+        .then( car => res.json(car))
+        .catch(next);
 });
 
+router.post("/",
+    checkCarPayload,
+    checkVinNumberValid,
+    checkVinNumberUnique,
+
+    (req, res, next) => {
+        Cars.create(req.body)
+            .then( newCar => res.status(201).json(newCar))
+            .catch(next);
+    }
+);
 
 module.exports = router;
