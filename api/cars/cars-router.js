bbb441ca461e72e21de492bc76fd4f4386d08478
shanny2022 <115324786+shanny2022@users.noreply.github.com>
2023-12-26 01:12:27 -0500

    Update cars-router.js

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -18,7 +18,22 @@ router.get("/:id", checkCarId, (req, res, next) => {
         .then( car => res.json(car))
         .catch(next);
 });
-
+router.post('/', async (req, res) => {
+    const { vin, make, model, mileage } = req.body;
+    if (!vin || !make || !model || !mileage) {
+      return res.status(400).json({ message: 'vin, make, model and mileage are required' });
+    }
+    try {
+      const existingCar = await Cars.getByVin(vin);
+      if (existingCar) {
+        return res.status(400).json({ message: `vin ${vin} already exists` });
+      }
+      const newCar = await Cars.create(req.body);
+      res.status(201).json(newCar);
+    } catch (err) {
+      res.status(500).json({ message: 'Failed to create new car' });
+    }
+  });
 router.post("/",
     checkCarPayload,
     checkVinNumberValid,
