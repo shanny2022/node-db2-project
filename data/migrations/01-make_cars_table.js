910ae33c96123c89f367848569478c1d98c451cd
shanny2022 <115324786+shanny2022@users.noreply.github.com>
2023-12-26 01:00:22 -0500

    Update 01-make_cars_table.js

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,15 +1,15 @@
-exports.up = function(knex) {
-  return knex.schema.createTable('cars', table => {
-    table.increments();
-    table.string('vin').unique().notNullable();
-    table.string('make').notNullable();
-    table.string('model').notNullable();
-    table.integer('mileage').notNullable();
-    table.string('title');
-    table.string('transmission');
-  });
+exports.up = async function (knex) {
+  await knex.schema.createTable("cars", table => {
+    table.increments("id");
+    table.string("vin").notNullable().unique();
+    table.string("make").notNullable();
+    table.string("model").notNullable();
+    table.integer("mileage").notNullable();
+    table.string("title");
+    table.string("transmission");
+  })
 };
 
-exports.down = function(knex) {
-  return knex.schema.dropTableIfExists('cars');
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists("cars");
 };
